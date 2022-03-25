34dcc27cf55cd4ccbd46a24847e6dc8b4a8ff65b
schlenkibus <justus.scheil@parteimail.de>
2022-03-25 23:35:31 +0100

    added kinda even mosaic mode

--- group_similar.py
+++ group_similar.py
@@ -3,6 +3,78 @@ import sys
 import os
 from PIL import Image, ImageFilter
 import bordercrop
+import random
+
+def getTotalHeightOfBucket(bucket):
+    totalHeight = 0
+    for segment in bucket:
+        totalHeight += segment.size[1]
+    return totalHeight
+
+
+def getMaxTotalHeight(buckets):
+    maxHeight = 0
+    for bucket in buckets:
+        maxHeight = max(maxHeight, getTotalHeightOfBucket(bucket))
+    return maxHeight
+
+def storeImage(segment, outputPath, suffix):
+    name = outputPath.split(".")[0]
+    pwd = os.getcwd()
+    segment.save(pwd + name + suffix + ".png")
+
+def stichEvenRows(segments, outputImagePath, singleSize, numbuckets, shuffle=False, seed=187):
+
+    sortedSegments = segments
+
+    if shuffle:
+        random.seed(seed)
+        random.shuffle(sortedSegments)
+    else:
+        sortedSegments = sorted(segments, key=lambda x: x.size[1])
+
+    buckets = []
+
+    #create buckets
+    for i in range(numbuckets):
+        buckets.append([])
+    
+    for segment in sortedSegments:
+        #determine current max height of the buckets
+        maxHeight = getMaxTotalHeight(buckets)
+
+        #find the bucket that has the most difference to the current max height
+        maxDiff = 0
+        maxDiffIndex = 0
+        for index, bucket in enumerate(buckets):
+            height = getTotalHeightOfBucket(bucket)
+            diff = abs(height - maxHeight)
+            if diff > maxDiff:
+                maxDiff = diff
+                maxDiffIndex = index
+
+        #add segment to bucket
+        buckets[maxDiffIndex].append(segment)
+
+    maxHeight = 0
+    for bucket in buckets:
+        totalHeight = 0
+        for segment in bucket:
+            totalHeight += segment.size[1]
+        maxHeight = max(maxHeight, totalHeight)
+
+    #create image to hold all contents of the buckets
+    newImg = Image.new(('RGB'), (singleSize * numbuckets, maxHeight))
+    
+    #paste bucket into new image
+    for index, bucket in enumerate(buckets):
+        y = 0
+        for segment in bucket:
+            newImg.paste(segment, (index * singleSize, y))
+            y += segment.size[1]
+
+    #save image
+    storeImage(newImg, outputImagePath, "_evenRows")
 
 def stichAndSave(sortedSegments, suffix, outputImagePath, wSegments, hSegments, singleSize):
     newImg = Image.new(('RGB'), (wSegments * singleSize, hSegments * singleSize))
@@ -15,15 +87,12 @@ def stichAndSave(sortedSegments, suffix, outputImagePath, wSegments, hSegments, 
         y = int(index / wSegments) * singleSize
         newImg.paste(segment, (x, y))
 
-    name = outputImagePath.split(".")[0]
-    newImg.save(name + suffix + ".png")        
+    storeImage(newImg, outputImagePath, suffix)
 
 def cropBlackbarsFromImage(segment, precrop, blackThreshold=1, rowThreshold=0.95, minRowsForBorder=3):
     preprocessed = segment.crop((0, precrop, segment.size[0], segment.size[1] - precrop))
     borders = bordercrop.borders(preprocessed, blackThreshold, segment.size[0] * rowThreshold, minRowsForBorder)
-
     return preprocessed.crop(borders)
-
 
 def main(argv):
     imageSectionSize = 0
@@ -60,9 +129,6 @@ def main(argv):
     singleSize = imageSectionSize
     wSegments = int(width / singleSize)
     hSegments = int(height / singleSize)
-    totalSegments = wSegments * hSegments
-
-    print(f"totalSegments: {totalSegments}")
 
     #create a list of all the segments
     segments = []
@@ -79,18 +145,18 @@ def main(argv):
         if cropped.size[1] > 10:  
             croppedSegments.append(cropped)
 
-    print("croppedSegments: ", len(croppedSegments))
+    stichEvenRows(croppedSegments, outputImagePath, singleSize, numbuckets=10, shuffle=True)
 
     #sort segments by their height
-    sortedSegments = sorted(croppedSegments, key=lambda x: x.size[1])
+    sortedSegments = sorted(segments, key=lambda x: x.size[1])
     #paste images into one horizontal image and paste segements
     stichAndSave(sortedSegments, "_horizontal", outputImagePath, len(sortedSegments), 1, singleSize)
-    return 0
 
     #sort segments based on the color temperature
-    sortedSegments = sorted(segments, key=lambda x: x.getpixel((0,0)))
+    sortedSegments = sorted(sortedSegments, key=lambda x: x.getpixel((0,0)))
     #stich image together
     stichAndSave(sortedSegments, "_temperature", outputImagePath, wSegments, hSegments, singleSize)
+
     #sort all segments again based on their amount of black pixels
     sortedSegments = sorted(sortedSegments, key=lambda x: x.getbbox()[3])
     stichAndSave(sortedSegments, "_black-pixels", outputImagePath, wSegments, hSegments, singleSize)
